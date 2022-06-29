6184b9941ecc4230fda73a12ceafb0228104ec37
Manan Jain <mananj11@gmail.com>
2022-06-29 20:18:26 +0530

    Initial Commit

---
+++ ImageSteganographyLibrary/src/main/java/com/manan/imagesteganographylibrary/Text/AsyncTaskCallback/TextEncodingCallback.java
@@ -0,0 +1,15 @@
+package com.manan.imagesteganographylibrary.Text.AsyncTaskCallback;
+
+import com.manan.imagesteganographylibrary.Text.ImageSteganography;
+
+/**
+ * This the callback interface for TextEncoding AsyncTask.
+ */
+
+public interface TextEncodingCallback {
+
+    void onStartTextEncoding();
+
+    void onCompleteTextEncoding(ImageSteganography result);
+
+}
